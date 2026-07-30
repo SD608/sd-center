a424b3ad282880305996402b5a88f52d44ca11d4
SD608 <sd42837040@gmail.com>
2026-07-31 05:46:24 +0900

    Add files via upload

--- assets/js/extensions-data.js
+++ assets/js/extensions-data.js
@@ -1,3 +1,5 @@
+"use strict";
+
 window.SD_EXTENSION_PACKS = [
   {
     id: "sd-slot",
@@ -8,7 +10,7 @@ window.SD_EXTENSION_PACKS = [
     icon: "assets/icons/slot.png",
     fileName: "SDSlot_Stage7_Desktop.zip",
     downloadUrl: "downloads/extensions/SDSlot_Stage7_Desktop.zip",
-    description: "SD지갑 가상계좌와 연동되는 3릴 슬롯머신 시뮬레이션입니다.",
+    description: "SD지갑 가상계좌와 연동되는 3릴 슬롯머신 시뮬레이션입니다. 선결제 후 3개 릴이 일치하면 가상 당첨금을 지급합니다.",
     requirements: "SD종합센터 v2.1.1 이상",
     updatedAt: "2026-07-31",
     tags: ["SD지갑 연동", "ZIP 앱 추가", "가상 시뮬레이션"],
@@ -23,7 +25,7 @@ window.SD_EXTENSION_PACKS = [
     icon: "assets/icons/mukjippa.png",
     fileName: "SDMukjippa_Version1_1_Desktop.zip",
     downloadUrl: "downloads/extensions/SDMukjippa_Version1_1_Desktop.zip",
-    description: "SD지갑 가상계좌와 연동되는 묵찌빠 연승 도전 게임입니다. 승리 후 정산하거나 다음 연승에 도전할 수 있습니다.",
+    description: "SD지갑 가상계좌와 연동되는 연승 도전형 묵찌빠 시뮬레이션입니다. 승리 후 보상을 정산하거나 다음 연승에 도전할 수 있습니다.",
     requirements: "SD종합센터 v2.1.1 이상",
     updatedAt: "2026-07-31",
     tags: ["SD지갑 연동", "연승 도전", "ZIP 앱 추가"],
