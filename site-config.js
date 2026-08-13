f97074927d0195fe02f7bf8dc0e54974ef2927d5
SD608 <sd42837040@gmail.com>
2026-08-14 06:13:34 +0900

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,11 +1,11 @@
 window.SD_SITE_CONFIG = {
   productName: "SD종합센터",
-  version: "2.1.1",
+  version: "2.1.2",
   releaseLabel: "Distribution",
-  updatedAt: "2026-07-31",
+  updatedAt: "2026-08-14",
   fileName: "SDCenterSetup.exe",
-  downloadUrl: "https://github.com/SD608/sd-center/releases/download/v.2.1.1/SDCenterSetup.exe",
-  releasePageUrl: "https://github.com/SD608/sd-center/releases/tag/v.2.1.1",
+  downloadUrl: "https://github.com/SD608/sd-center/releases/download/v.2.1.2/SDCenterSetup.exe",
+  releasePageUrl: "https://github.com/SD608/sd-center/releases/tag/v.2.1.2",
   sourcePageUrl: "https://github.com/SD608/sd-center",
   systemRequirement: "Windows 10/11 64비트"
 };
