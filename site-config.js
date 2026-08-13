ecc7cd69827045d728ffc7ec725610995ff810c8
SD608 <sd42837040@gmail.com>
2026-08-14 00:44:34 +0900

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -4,8 +4,8 @@ window.SD_SITE_CONFIG = {
   releaseLabel: "Distribution",
   updatedAt: "2026-07-31",
   fileName: "SDCenterSetup.exe",
-  downloadUrl: "https://github.com/SD608/sd-center/releases/latest/download/SDCenterSetup.exe",
-  releasePageUrl: "https://github.com/SD608/sd-center/releases/tag/v2.1.1",
+  downloadUrl: "https://github.com/SD608/sd-center/releases/download/v.2.1.1/SDCenterSetup.exe",
+  releasePageUrl: "https://github.com/SD608/sd-center/releases/tag/v.2.1.1",
   sourcePageUrl: "https://github.com/SD608/sd-center",
   systemRequirement: "Windows 10/11 64비트"
 };
