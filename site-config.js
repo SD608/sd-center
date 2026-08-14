35d8bc37b21c6011c6b852b1f9d2575435916c58
SD608 <sd42837040@gmail.com>
2026-08-14 21:43:03 +0900

    site: point Windows download to SDCenter v2.1.4

--- site-config.js
+++ site-config.js
@@ -1,11 +1,11 @@
 window.SD_SITE_CONFIG = {
   productName: "SD종합센터",
-  version: "2.1.3",
+  version: "2.1.4",
   releaseLabel: "Distribution",
   updatedAt: "2026-08-14",
   fileName: "SDCenterSetup.exe",
-  downloadUrl: "https://github.com/SD608/sd-center/releases/download/v.2.1.3/SDCenterSetup.exe",
-  releasePageUrl: "https://github.com/SD608/sd-center/releases/tag/v.2.1.3",
+  downloadUrl: "https://github.com/SD608/sd-center/releases/download/v.2.1.4/SDCenterSetup.exe",
+  releasePageUrl: "https://github.com/SD608/sd-center/releases/tag/v.2.1.4",
   sourcePageUrl: "https://github.com/SD608/sd-center",
   systemRequirement: "Windows 10/11 64비트"
 };
